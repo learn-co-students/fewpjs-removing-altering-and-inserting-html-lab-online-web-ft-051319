c0413430c604fd71239ab79d9e4384c60b03d88b
M Banks <montensia1@outlook.com>
2019-08-16 14:21:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code here!
+
+// var element = document.createElement('div');
+
+// document.body.appendChild(element);
+
+// var ul = document.createElement('ul');
+
+// for(let i = 1; i <= 3; i++){
+//   let li = document.createElement('li');
+//   li.innerHTML = (i).toString();
+//   ul.appendChild(li);
+// }
+
+// var para = document.createElement("p#greeting");
+// element.appendChild(0);
+
+// let element2 = document.querySelector("p#greeting");
+
+// element2.innerHTML = "Hello, DOM!";
+
+// let header = document.getElementById("div#header");
+
+// header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+// element.style.backgroundColor = '#27647B';
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left';
+
+// element.className = "dog";
+// element.className = "pet-listing dog";
+document.querySelector("main#main").remove();
+let newHeader = document.createElement('h1');
+//newHeader.setAttribute("id", "victory");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
