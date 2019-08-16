6453d1d3ba04c7dd2fd614ed5382d5dca3b8424b
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-08-16 12:22:19 +0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+
+var newHeader=document.createElement('h1')
+newHeader.id='victory'
+newHeader.innerHTML="Boon Sim is the champion"
+document.body.appendChild(newHeader)
