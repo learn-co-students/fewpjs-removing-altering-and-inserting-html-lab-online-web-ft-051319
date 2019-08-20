85ba50b48ae1c74198ca23ddebd2f9a567b5a32f
Natalie <natsetc@gmail.com>
2019-08-19 18:02:51 -0700

    notes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 main.remove()
+// document.querySelector("main#main").remove();
 
 let newHeader = document.createElement('h1')
-document.body.appendChild(newHeader)
+// document.body.appendChild(newHeader)
 
 newHeader.id = "victory"
 newHeader.innerHTML = "YOUR-NAME is the champion"
