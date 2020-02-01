15cfe40ead86fcbb1418188ee0fabe9d2c8dad4e
Brian Salemi <briandsalemi@gmail.com>
2020-02-01 16:32:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Write your code here!
+let mainNode = document.querySelector('#main')
+mainNode.remove()
 
-let main_element = document.querySelector('#main');
-
-main_element.remove();
-
-let newHeader = document.createElement('h1'); 
-newHeader.id = "victory";
-newHeader.innerHTML = "YOUR-NAME is the champion";
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
